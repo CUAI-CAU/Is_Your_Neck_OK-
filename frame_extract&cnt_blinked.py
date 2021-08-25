299329574c59102170b62f64b7112d27d6e46817
sksmslhy <tbwn0406@naver.com>
2021-08-25 21:07:03 +0900

    add mkdir code

--- frame_extract&cnt_blinked.py
+++ frame_extract&cnt_blinked.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
+import errno
 import cv2
 import time
-import dlib   # version = 19.22.0
+import dlib  # version = 19.22.0
 from scipy.spatial import distance as dist
 import datetime
 import numpy as np
+import os
 
 '''
     - output
         : "./dataset/inputN.jpg" -> 60장
         : "./blink_count.txt" -> int, 3분 간 눈 깜박임 횟수
-        
+
 '''
 
 JAWLINE_POINTS = list(range(0, 17))
@@ -30,19 +32,19 @@ TOTAL = 0
 
 
 def eye_aspect_ratio(eye):
-  # euclidean distance between the vertical eye landmarks
-  A = dist.euclidean(eye[1], eye[5])
-  B = dist.euclidean(eye[2], eye[4])
+    # euclidean distance between the vertical eye landmarks
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
 
-  # euclidean distance between the horizontal eye landmarks
-  C = dist.euclidean(eye[0], eye[3])
+    # euclidean distance between the horizontal eye landmarks
+    C = dist.euclidean(eye[0], eye[3])
 
-  ear = (A + B) / (2 * C)
-  return ear
+    ear = (A + B) / (2 * C)
+    return ear
 
 
 def recording(cap, fps, codec, count, start_time, COUNTER, TOTAL, cnt, start_t):
-    while(cap.isOpened()):
+    while (cap.isOpened()):
 
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)  # 화면 반전 0: 상하, 1: 좌우
@@ -86,7 +88,8 @@ def recording(cap, fps, codec, count, start_time, COUNTER, TOTAL, cnt, start_t):
                 cv2.imwrite("./dataset/input%d.jpg" % cnt, frame)
                 img = cv2.imread("./dataset/input%d.jpg" % cnt)
                 # 중앙 기준으로 정사각형 모양으로 사진 자름
-                res = img[0:img.shape[0], int((img.shape[1] / 2) - (img.shape[0] / 2)):int((img.shape[1] / 2) + (img.shape[0] / 2))].copy()
+                res = img[0:img.shape[0],
+                      int((img.shape[1] / 2) - (img.shape[0] / 2)):int((img.shape[1] / 2) + (img.shape[0] / 2))].copy()
                 cv2.imwrite("./dataset/input%d.jpg" % cnt, res)
                 print("saved image%d.jpg" % cnt)
                 cnt += 1
@@ -94,8 +97,8 @@ def recording(cap, fps, codec, count, start_time, COUNTER, TOTAL, cnt, start_t):
 
             if diff > 186:  # 3분 동안 웹캠에서 프레임 추출
                 break
-            #k = cv2.waitKey(1) & 0xFF
-            #if k == 27:  # ESC 키 눌러졌을 경우 종료
+            # k = cv2.waitKey(1) & 0xFF
+            # if k == 27:  # ESC 키 눌러졌을 경우 종료
             #    break
         else:
             break
@@ -105,6 +108,15 @@ def recording(cap, fps, codec, count, start_time, COUNTER, TOTAL, cnt, start_t):
 
 
 if __name__ == '__main__':
+
+    try:
+        if not (os.path.isdir('./dataset')):
+            os.makedirs(os.path.join('dataset'))
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            print("Failed to create directory!")
+            raise
+
     cap = cv2.VideoCapture(0)
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor('./model/shape_predictor_68_face_landmarks.dat')
